e20d2cb361b615bed8de78a971a3a7793357cb00
Pavel Redics <pavel.redics@gmail.com>
2014-11-09 11:11:36 +0000

    modify tests

--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -2,19 +2,25 @@ require 'my_inject'
 
 describe 'my_inject' do
 
-  it 'print sum of 6 elements' do
-    expect([10, 23, 32, 10, 20, 30].my_inject(10) {|sum, num| sum+num }).to eq 135
+  it 'print reslut of sum of elements' do
+    expect([10, 23, 32, 21, 30].my_inject(10) { |sum, num| sum + num }).to eq 126
   end
 
-  it 'print diff of two elements' do
-    expect([10, 23, -13].my_inject(10) {|diff, num| diff-num }).to eq -10
+  it 'print result of diff of elements' do
+    expect([10, 23, -13].my_inject { |diff, num| diff - num }).to eq 0
   end
 
-  it 'print multiply result of three elements' do
-    expect([10, 23, 10].my_inject(2) {|mult, num| mult*num }).to eq 4600
+  it 'print result of multiply of elements' do
+    expect([10, 5, 3].my_inject(2) { |mult, num| mult * num }).to eq 300
   end
 
-  it 'print division result of two' do
-    expect([100, 10, 10].my_inject {|mult, num| mult/num }).to eq 1   
+  it 'print result of division of elements' do
+    expect([27, 3, 9].my_inject { |mult, num| mult / num }).to eq 1   
+  end
+
+  it 'print longest word in array' do
+    expect(%w{ donkey dog sheep }.my_inject do |memo, word|
+      memo.length < word.length ? memo : word
+    end).to eq "dog"
   end
 end
