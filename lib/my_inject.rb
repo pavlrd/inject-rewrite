f4f9aadd4db5b5d7d265e1e8457cfebce1db274a
Pavel Redics <pavel.redics@gmail.com>
2014-11-09 01:50:09 +0000

    working inject

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,9 +1,20 @@
 class Array
 
- def my_inject() 
-  first = self[0]
-  second = self[1] 
-  yield(first, second)
+ def my_inject(accumulator = nil) 
+  result = 0
+  i = accumulator == nil ? 1 : 0
+  accumulator = accumulator == nil ? self.first : accumulator
+  if block_given?
+    while i < self.length
+    number = self[i]
+    result = yield(accumulator, number)
+    accumulator = result
+    i += 1
+  end
+  end
+  accumulator
  end
  
 end
+
+# a lot refactoring needs to be done, but it's working so far
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -2,19 +2,19 @@ require 'my_inject'
 
 describe 'my_inject' do
 
-  it 'print sum of two elements' do
-    expect([10, 23].my_inject {|sum, num| sum+num }).to eq 33
+  it 'print sum of 6 elements' do
+    expect([10, 23, 32, 10, 20, 30].my_inject(10) {|sum, num| sum+num }).to eq 135
   end
 
   it 'print diff of two elements' do
-    expect([10, 23].my_inject {|diff, num| diff-num }).to eq -13
+    expect([10, 23, -13].my_inject(10) {|diff, num| diff-num }).to eq -10
   end
 
-  it 'print multiply result of two elements' do
-    expect([10, 23].my_inject {|mult, num| mult*num }).to eq 230
+  it 'print multiply result of three elements' do
+    expect([10, 23, 10].my_inject(2) {|mult, num| mult*num }).to eq 4600
   end
 
   it 'print division result of two' do
-    expect([100, 10].my_inject {|mult, num| mult/num }).to eq 10   
+    expect([100, 10, 10].my_inject {|mult, num| mult/num }).to eq 1   
   end
 end
