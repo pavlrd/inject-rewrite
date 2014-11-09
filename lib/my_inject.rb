ae3d7e28fdd89b21b39cc0560f8cc149d280598e
Pavel Redics <pavel.redics@gmail.com>
2014-11-09 12:05:23 +0000

    refactor inject

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,20 +1,16 @@
 class Array
 
  def my_inject(accumulator = nil) 
-  result = 0
   i = accumulator == nil ? 1 : 0
   accumulator = accumulator == nil ? self.first : accumulator
   if block_given?
-    while i < self.length
-    number = self[i]
-    result = yield(accumulator, number)
-    accumulator = result
-    i += 1
-  end
+    self[i..-1].each do |element|
+      result = yield(accumulator, element)
+      accumulator = result
+    end
   end
   accumulator
  end
- 
 end
 
-# a lot refactoring needs to be done, but it's working so far
+
