aeddee04268ea3269bad9f0097e542dcabfb0e0f
Pavel Redics <pavel.redics@gmail.com>
2014-11-09 22:20:39 +0000

    split different approaches into separate files

---
+++ lib/inject_with_recursion.rb
@@ -0,0 +1,10 @@
+class Array
+
+ def my_inject(accumulator = nil, &block)
+  return yield(accumulator, self.shift) if self.length == 1 && accumulator != nil
+  return yield(self.shift, self.shift) if self.length == 2 && accumulator == nil
+  accumulator = self.shift if accumulator.nil?
+  number = accumulator.nil? ? self[1] : self.shift
+  self.my_inject(yield(accumulator, number), &block)
+end
+end
