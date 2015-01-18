7a1fa3c1580a841aa9b3c46ea239c85fb792d1bc
Pavel Redics <pavel.redics@gmail.com>
2015-01-18 22:59:45 +0000

    fix indentation

--- lib/my_inject_with_iterator.rb
+++ lib/my_inject_with_iterator.rb
@@ -1,15 +1,15 @@
 class Array
 
- def my_inject_iterator(accumulator = nil)
-  return nil if self.empty? && accumulator == nil
-  i = accumulator == nil ? 1 : 0
-  accumulator = self.first if accumulator.nil?
-  self[i..-1].each do |element|
-    result = yield(accumulator, element)
-    accumulator = result
-  end
-  accumulator
- end
+  def my_inject_iterator(accumulator = nil)
+    return nil if self.empty? && accumulator == nil
+    i = accumulator == nil ? 1 : 0
+    accumulator = self.first if accumulator.nil?
+    self[i..-1].each do |element|
+      result = yield(accumulator, element)
+      accumulator = result
+    end
+    accumulator
+   end
 end
 
 
--- lib/my_inject_with_recursion.rb
+++ lib/my_inject_with_recursion.rb
@@ -1,15 +1,15 @@
 class Array
 
- def my_inject_recursion(accumulator = nil, &block)
-  return nil if self.empty? && accumulator == nil
-  return accumulator if !accumulator.nil? && self.empty?
-  return self.first if accumulator.nil? && self.length == 1
-  object = self.dup
-  return yield(accumulator, object.shift) if object.length == 1 && accumulator != nil
-  return yield(object.shift, object.shift) if object.length == 2 && accumulator == nil
-  accumulator = object.shift if accumulator.nil?
-  number = accumulator.nil? ? object[1] : object.shift
-  object.my_inject_recursion(yield(accumulator, number), &block)
+  def my_inject_recursion(accumulator = nil, &block)
+    return nil if self.empty? && accumulator == nil
+    return accumulator if !accumulator.nil? && self.empty?
+    return self.first if accumulator.nil? && self.length == 1
+    object = self.dup
+    return yield(accumulator, object.shift) if object.length == 1 && accumulator != nil
+    return yield(object.shift, object.shift) if object.length == 2 && accumulator == nil
+    accumulator = object.shift if accumulator.nil?
+    number = accumulator.nil? ? object[1] : object.shift
+    object.my_inject_recursion(yield(accumulator, number), &block)
 end
 end
 
