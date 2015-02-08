f9c89d858f54d9f4aa9d70018b5d22ec410653dc
Pavel Redics <palyrex@users.noreply.github.com>
2015-02-08 20:37:08 +0000

    comments

--- lib/inject_recursion.rb
+++ lib/inject_recursion.rb
@@ -13,7 +13,8 @@ class Array
     @initial = @dupl_self.shift if initial_not_given?
     initial_not_given? ? @number = self.first : @number = @dupl_self.shift
 	end
-
+	
+	# recursion happens here when previos cases been satisfied 
 	def recursion(&block)
     @dupl_self.my_inject_recursion(yield(@initial, @number), &block)
 	end
@@ -30,17 +31,20 @@ class Array
 		return nil if initial_not_given?
 		return @initial
 	end
-
+  
+  # checking edge cases when given array empty or one number without accumulator 
   def edge_cases
     self.empty? || self_one_number_no_initial
   end
 
+  # give result based on edge cases
   def result_edge_cases(&block)
     return result_when_self_empty if self.empty?
     return self.first if self_one_number_no_initial
     return yield(self[0], self[1]) if self_two_numbers_no_initial
   end
 
+  # main inject method
   def my_inject_recursion(initial = "", &block)
   	setup_inject(initial)
 		return yield(self[0], self[1]) if self_two_numbers_no_initial
